ea7726db7248a2fd32cdf8f316e0fec1a87324e9
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-07 22:20:50 -0500

    created contact page

--- antony-portfolio/src/components/pages/Home.js
+++ antony-portfolio/src/components/pages/Home.js
@@ -1,2 +1,14 @@
 import React from 'react';
 
+export default function Home() {
+    return (
+      <React.Fragment>
+        <h1>Antony Quinones</h1>
+        <img id="profile-picture" src="" alt="profile-picture"/>
+        <h1>Hello! Welcome to my portfolio.</h1>
+        <p>
+        This is my bio.
+        </p>
+      </React.Fragment>
+    );
+  }
