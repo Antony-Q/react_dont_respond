db44cc8b6bc4500543472ab0feeb2124475f09c8
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-07 21:59:28 -0500

    created contact page

---
+++ antony-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div className="card">
+            <h1>Contact Me</h1>
+            <div className="contact-info" >
+                <h3>Phone: 210-508-7291</h3>
+                <h3>Email: antonyq1992@gmail.com</h3>
+                <a href="https://github.com/Antony-Q">GitHub</a>
+                <a href="https://www.linkedin.com/in/antony-quinones-086a91227/">LinkedIn</a>
+            </div>
+        </div>
+
+    )
+}
+
+module.exports = Contact
